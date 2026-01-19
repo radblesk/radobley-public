0832cb1bafbb793c471c876fe38ec493cdef9760
radblesk <144258807+radblesk@users.noreply.github.com>
2026-01-19 20:25:57 +0100

    feat(localizations): added prefix configuration
    Signed-off-by: radblesk <144258807+radblesk@users.noreply.github.com>

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,6 +4,13 @@ export const routing = defineRouting({
   // A list of all locales that are supported
   locales: ["en"],
 
+  // Displays prefix on non-default locales (e.g., "/about ... /sk/about")
+  localePrefix: "as-needed",
+
   // Used when no locale matches
   defaultLocale: "en",
+
+  pathnames: {
+    "/": "/",
+  },
 });
--- proxy.ts
+++ proxy.ts
@@ -4,8 +4,13 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    "/((?!api|_next|_vercel|.*\\..*).*)",
+
+    // However, match all pathnames within `/users`, optionally with a locale prefix
+    "/([\\w-]+)?/users/(.+)",
+  ],
 };
