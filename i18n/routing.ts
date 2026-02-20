c9228c3b19f0c93e49f7d0b0d129bdc345854cc0
radblesk <144258807+radblesk@users.noreply.github.com>
2026-02-20 21:03:00 +0100

    feat(localizations): added default slovak locale, added new strings

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,13 +2,13 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en"],
+  locales: ["sk"],
 
   // Displays prefix on non-default locales (e.g., "/about ... /sk/about")
   localePrefix: "as-needed",
 
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale: "sk",
 
   pathnames: {
     "/": "/",
