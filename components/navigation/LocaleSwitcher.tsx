211ebc1c6c7aeef8279952b7156817ba6d06be6f
radblesk <144258807+radblesk@users.noreply.github.com>
2026-03-05 19:56:46 +0100

    feat(components): reworked fully custom language switcher

--- components/navigation/LocaleSwitcher.tsx
+++ components/navigation/LocaleSwitcher.tsx
@@ -1,5 +1,6 @@
 import styles from "./localeswitcher.module.scss";
 import { Locale } from "@/helpers/locales";
+import { useEffect, useRef, useState } from "react";
 
 type LocaleSwitcherProps = {
   current: Locale;
@@ -10,17 +11,49 @@ export default function LocaleSwitcher({
   current,
   action,
 }: LocaleSwitcherProps) {
+  // States
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Ref
+  const ref = useRef<HTMLDivElement>(null);
+
+  // Functions
+  function toggleDropdown() {
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node))
+        setIsOpen(false);
+    };
+    document.addEventListener("mousedown", handler);
+    return () => document.removeEventListener("mousedown", handler);
+  }, []);
+
   return (
-    <select
-      className={styles.switcher}
-      value={current}
-      onChange={(e) => action(e.target.value as Locale)}
-    >
-      {Object.entries(Locale).map(([key, value]) => (
-        <option key={key} value={key}>
-          {value.toUpperCase()}
-        </option>
-      ))}
-    </select>
+    <div ref={ref} className={styles.switcher}>
+      <button className={isOpen ? styles.isOpen : ""} onClick={toggleDropdown}>
+        {current}
+      </button>
+
+      {isOpen && (
+        <ul>
+          {Object.entries(Locale).map(([key, value]) => (
+            <li key={key}>
+              <button
+                disabled={value === current}
+                onClick={() => {
+                  action(value);
+                  toggleDropdown();
+                }}
+              >
+                {value}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
